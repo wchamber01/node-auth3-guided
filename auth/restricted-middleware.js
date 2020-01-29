d34804e093bdd1435e7c94cc400e1aa514cffbbe
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-29 10:42:32 +0100

    wip

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
@@ -9,7 +9,20 @@ module.exports = (req, res, next) => {
     // it's made by us (was made using the secret)
     // the token hasn't expired
     // hasn't been tampered with etc
-    
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'thesecret',
+      // callback (err, decodedToken)
+      (err, decoded) => {
+        if (err) {
+          res.status(401).json({ message: 'token bad' })
+        } else {
+          // let's put the decoded token on req
+          req.decodedToken = decoded;
+          next();
+        }
+      }
+    )
   } else {
     res.status(400).json({ message: 'No credentials provided' });
   }

fcc4f5bb7735ca9f429dae82bcd4705b127dcbcf
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-29 10:37:50 +0100

    wip

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,7 +3,13 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  if () {
+  const token = req.headers.authorization;
+  if (token) {
+    // verify is it valid?
+    // it's made by us (was made using the secret)
+    // the token hasn't expired
+    // hasn't been tampered with etc
+    
   } else {
     res.status(400).json({ message: 'No credentials provided' });
   }
